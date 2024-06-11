3c86de13d9edba46686f4fbced1642f686d223a5
Saket Jain <jsaket058@gmail.com>
2024-06-12 01:57:39 +0530

    okk

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import { Route , Routes } from 'react-router-dom'
+import Header from './components/header/Header'
+import Home from './pages/home/Home'
+import Footer from './components/footer/Footer'
+import NotFound from './pages/404/NotFound'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="*" element={<NotFound/>}/>
+    </Routes>  
+    <Footer/>
+    </>
   );
-}
+};
 
-export default App;
+export default App
---
+++ src/pages/home/Home.js
@@ -0,0 +1,22 @@
+import "./Home.scss";
+
+
+const Home = () => {
+ const name = "Saket Jain"
+ return <div className="home">
+  <div className="container">
+    <h2>Home Page</h2>
+    <p>
+      Welcome to the <b>Home</b> page
+    </p>
+    <p>
+      This page has <b>Redux Toolkit</b> set up.
+    </p>
+    <p>
+      The name: <b>{name}</b> is stored in the dataSlice
+    </p>
+  </div>
+ </div>;
+};
+
+export default Home
